f04ebb9034903be3a19837c81ca3db26be1c8881
Gabriel Penna <gabriel.penna@tce.mg.gov.br>
2025-12-10 16:16:11 -0300

    Miniblog inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRoute, Routes, Route, Navigate, BrowserRouter} from 'react-router-dom';
+
+import Home from './pages/Home/Home.js';
+import About from './pages/About/About.js';
+import Footer from './components/Footer.js';
+import Navbar from './components/Navbar.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+      <div className= "container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          
+        </Routes>
+      </div>
+      <Footer />
+      </BrowserRouter>     
     </div>
   );
 }
